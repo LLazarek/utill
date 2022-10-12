14c9fbdada2da03b38548c51cecda171692e2d98
Lukas Lazarek <LukasALazarek@gmail.com>
2022-10-12 12:44:08 -0500

    Finish repl

--- repl.rkt
+++ repl.rkt
@@ -1,5 +1,39 @@
 #lang racket/base
 
-(require scribble/reader)
+(require scribble/reader
+         readline/readline
+         syntax/parse/define)
 
-(provide (rename-out [use-at-readtable enable-at-exp!]))
+(provide (rename-out [use-at-readtable enable-at-exp!])
+         make-repl)
+
+(define-simple-macro (make-repl
+                      prompt
+                      #:input input-name
+                      #:loop
+                      ([loop-pat loop-body ...] ...)
+                      #:exit
+                      ([exit-pat exit-body ...] ...))
+  (let loop ()
+    (display prompt)
+    (flush-output)
+    (define input-name (readline prompt))
+    (when (and (string? input-name)
+               (not (string-prefix? input-name " ")))
+      (add-history input-name))
+    (match (string-trim input-name #:right? #f)
+      [(regexp "^h(elp)?")
+       (displayln "Available commands:")
+       (displayln (list loop-pat ... exit-pat ...))
+       (loop)]
+      [loop-pat
+       loop-body ...
+       (loop)]
+      ...
+      [exit-pat
+       exit-body ...]
+      ...
+      [(? eof-object?) (void)]
+      [else
+       (printf "Unknown command: ~a\n" input-name)
+       (loop)])))
