324be78b64822be45edb3f62a6349400429796c5
Lukas Lazarek <LukasALazarek@gmail.com>
2022-11-26 17:45:37 -0600

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "utill")
+(define deps '("base"
+               "rscript"))
+(define build-deps '("at-exp-lib"))
+(define pkg-desc "utils")
+(define pkg-authors '(lukas))
